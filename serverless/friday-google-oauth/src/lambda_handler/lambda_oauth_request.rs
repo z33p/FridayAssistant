29a3bb580be54eff2d2e96de6a0e04f03f3297e1
z33p <raphael00fellipe@gmail.com>
2023-12-16 01:11:36 -0300

    refac: remove unecessary serde rename

--- serverless/friday-google-oauth/src/lambda_handler/lambda_oauth_request.rs
+++ serverless/friday-google-oauth/src/lambda_handler/lambda_oauth_request.rs
@@ -2,8 +2,6 @@ use serde_derive::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct LambdaOAuthRequest {
-    #[serde(rename = "action")]
     pub action: String,
-    #[serde(rename = "data")]
     pub data: serde_json::Value,
 }
