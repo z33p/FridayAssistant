095e01041139776f878950ddb4a47cec15f8a8b4
z33p <raphael00fellipe@gmail.com>
2026-04-02 17:39:55 -0300

    fix: update pod name in port-forward script to match current deployment

--- serverless/friday-todo-manager/port-forward.sh
+++ serverless/friday-todo-manager/port-forward.sh
@@ -1,3 +1,3 @@
 # Description: Forward the port of the pod to the local machine
 # The pod name may change, so you need to get the pod name first
-kubectl port-forward pod/friday-todo-manager-688779b4c7-f9qf6 5000:5000 
+kubectl port-forward pod/friday-todo-manager-74f6c56d75-pjcdj 5000:5000 
