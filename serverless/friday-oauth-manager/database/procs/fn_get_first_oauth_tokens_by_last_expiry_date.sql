050d88e5354780f91c1c650b0955bc48842bf869
z33p <raphael00fellipe@gmail.com>
2026-04-06 21:52:22 -0300

    refactor: clean up function definition for fetching latest OAuth tokens

--- serverless/friday-oauth-manager/database/procs/fn_get_first_oauth_tokens_by_last_expiry_date.sql
+++ serverless/friday-oauth-manager/database/procs/fn_get_first_oauth_tokens_by_last_expiry_date.sql
@@ -1,19 +1,18 @@
 -- DROP FUNCTION fn_get_first_oauth_tokens_by_last_expiry_date;
 CREATE OR REPLACE FUNCTION fn_get_first_oauth_tokens_by_last_expiry_date() RETURNS TABLE (
-        id_oauth_tokens UUID,
-        id_provider INT,
-        access_token TEXT,
-        refresh_token TEXT,
-        expiry_date TIMESTAMP WITH TIME ZONE
-    ) AS $$ BEGIN RETURN QUERY
+    id_oauth_tokens UUID,
+    id_provider INT,
+    access_token TEXT,
+    refresh_token TEXT,
+    expiry_date TIMESTAMP WITH TIME ZONE
+) AS $$ BEGIN RETURN QUERY
 SELECT tb_oauth_tokens.id_oauth_tokens,
     tb_oauth_tokens.id_provider,
     tb_oauth_tokens.access_token,
     tb_oauth_tokens.refresh_token,
     tb_oauth_tokens.expiry_date
 FROM tb_oauth_tokens
-WHERE tb_oauth_tokens.expiry_date > NOW() -- Somente tokens que ainda não expiraram
-ORDER BY tb_oauth_tokens.expiry_date DESC -- O token que expira mais tarde (mais recente)
+ORDER BY tb_oauth_tokens.expiry_date DESC
 LIMIT 1;
 END;
 $$ LANGUAGE plpgsql;
